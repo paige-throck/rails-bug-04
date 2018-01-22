dece98742c68871e25d1ff835fc841853bc56fbf
Paige Throckmorton <path4942@gmail.com>
2018-01-22 16:27:25 -0600

    Fixed erro broke submit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "task_lists#index"
   get "signin" => "sessions#new", as: :signin
   post "signin" => "sessions#create"
+  post "tasks" => "tasks#create"
   get "signout" => "sessions#destroy", as: :signout
 
   resources :task_lists, only: [] do
