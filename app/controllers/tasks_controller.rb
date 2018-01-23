f053916734fada1fb6940ded0d7b3ee2b70a3331
Paige Throckmorton <path4942@gmail.com>
2018-01-23 08:58:22 -0600

    Fixed bug-04

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    task_params = params.require(:task).permit(:description, :due_date).merge(task_list_id: params[:task_list_id])
+    if params[:task_list_id]
+    session['id'] = params[:task_list_id]
+  end
+
+    task_params = params.require(:task).permit(:description, :due_date).merge(task_list_id: session['id'])
     @task = Task.new(task_params)
     if @task.save
       redirect_to root_path, notice: "Task was created successfully!"
